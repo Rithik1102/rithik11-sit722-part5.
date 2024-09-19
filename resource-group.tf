ddec138dd4bee3850ed5dc976336a9122472a1c5
Rithik <47662883+Rithik1102@users.noreply.github.com>
2024-09-19 12:56:35 +1000

    Update resource-group.tf

--- resource-group.tf
+++ resource-group.tf
@@ -1,7 +1,7 @@
 #
 # Creates a resource group for FlixTube in your Azure account.
 #
-resource "azurerm_resource_group" "Part4" {
+resource "azurerm_resource_group" "Part5" {
   name     = var.app_name
   location = var.location
 }
