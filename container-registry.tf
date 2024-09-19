18262329685e054022d218dc0f837240561e7822
Rithik <47662883+Rithik1102@users.noreply.github.com>
2024-09-19 12:57:14 +1000

    Update container-registry.tf

--- container-registry.tf
+++ container-registry.tf
@@ -3,7 +3,7 @@
 #
 resource "azurerm_container_registry" "container_registry" {
   name                = var.app_name
-  resource_group_name = azurerm_resource_group.Part4.name
+  resource_group_name = azurerm_resource_group.Part5.name
   location            = var.location
   admin_enabled       = true
   sku                 = "Basic"
