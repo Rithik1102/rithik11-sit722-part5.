d8b9d95633bb1b3b8f691d7bf6a550a1c2a7fa72
Rithik <47662883+Rithik1102@users.noreply.github.com>
2024-09-19 23:39:14 +1000

    Update push-image.sh

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -21,5 +21,9 @@ set -u # or set -o nounset
 : "$REGISTRY_PW"
 
 echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+docker tag $CONTAINER_REGISTRY/book_catalog:$VERSION
+docker tag $CONTAINER_REGISTRY/inventory_mangement:$VERSION
+
+
 docker push $CONTAINER_REGISTRY/book_catalog:$VERSION
 docker push $CONTAINER_REGISTRY/inventory_mangement:$VERSION
