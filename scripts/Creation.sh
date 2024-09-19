b13aa7913302916db9f5624c0b4009401caab5cf
Rithik <47662883+Rithik1102@users.noreply.github.com>
2024-09-19 13:05:00 +1000

    Update Creation.sh

--- scripts/Creation.sh
+++ scripts/Creation.sh
@@ -1,4 +1,4 @@
-$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; Remove-Item .\AzureCLI.msi
+
 az login --username odl_user_1410262@cloudlabs4deakin.onmicrosoft.com --password ikuh15BUB*ec
-terraform init
+
 terraform apply -auto-approval
