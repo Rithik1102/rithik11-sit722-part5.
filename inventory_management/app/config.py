6693596dbae585973e0c1dc42ac0bcd088f31ece
Rithik <47662883+Rithik1102@users.noreply.github.com>
2024-09-20 11:53:56 +1000

    Update config.py

--- inventory_management/app/config.py
+++ inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', ${{ secrets.DATABASE_URL }})
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://rithik11_sit722_part5_user:aMD0j1Lyl08F16yeDZ2mVirDeN1aPpKl@dpg-crm309rv2p9s73e7nc50-a.singapore-postgres.render.com/rithik11_sit722_part5')
